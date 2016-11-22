59a633610055b7831e1ca1242990af658d12da3f
John Children <john@cognitivelogic.com>
2016-11-22 12:52:43 +0000

    Update requirements and config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,14 +19,10 @@
 #
 import os
 # import sys
+from recommonmark.parser import CommonMarkParser
 # sys.path.insert(0, os.path.abspath('.'))
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- General configuration ------------------------------------------------
 
@@ -53,11 +49,13 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #
 # source_encoding = 'utf-8-sig'
+
+source_parsers = {'.md': CommonMarkParser, }
 
 # The master toctree document.
 master_doc = 'index'
@@ -129,11 +127,15 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
